6602b6941bdd39746163c8eafe3733b4ec168af0
jshishimaru <jbsss785@gmail.com>
2023-12-11 21:47:41 +0530

    adds basic commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,121 +1,136 @@
 #include <iostream>
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 #include <string>
-using namespace std;
 
+int WIDTH  =1280;
+int HEIGHT =720 ;
 SDL_Window* window = NULL;
-SDL_Surface* wsurface = NULL;
-SDL_Surface* images = NULL;
+SDL_Renderer* renderer= NULL;
 
-enum keypress{
+enum keypress
+{
      KEY_DEFAULT,
-     KEY_UP,
-     KEY_DOWN,
-     KEY_LEFT,
-     KEY_RIGHT,
-     KEY_TOTAL,
-};
+     KEY_W,
+     KEY_A,
+     KEY_S,
+     KEY_D,
+     KEY_SPACE,
 
-SDL_Surface* keysurface[KEY_TOTAL];
-
-bool init()
-{
-       bool initialized = true;
-       SDL_Init( SDL_INIT_EVERYTHING );
-       window=SDL_CreateWindow( "keypress" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , 1000 ,  1000 , SDL_WINDOW_RESIZABLE);
-       wsurface = SDL_GetWindowSurface( window );
-       return initialized;     
-}
-
-SDL_Surface* loadsurface( string path ) // loads the image at given path and returns loaded surface 
-{
-         SDL_Surface* optimized = NULL;
-         SDL_Surface* loaded= SDL_LoadBMP( path.c_str() );
-         if( loaded == NULL )
-	{
-		printf( "Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-	}
-        else
-        {
-            optimized = SDL_ConvertSurface( loaded , wsurface->format , 0 );
-        }
-         SDL_FreeSurface( loaded );
-         return optimized;
-}
-
-bool loadmedia()
-{
-     bool mediaload=true;
-     keysurface[KEY_DEFAULT]=loadsurface( "Assets/default.bmp");
-     keysurface[KEY_UP]= loadsurface( "Assets/up.bmp" );
-     keysurface[KEY_DOWN]=loadsurface( "Assets/down.bmp");
-     keysurface[KEY_LEFT]=loadsurface( "Assets/left.bmp");
-     keysurface[KEY_RIGHT]=loadsurface( "Assets/right.bmp");
-     return mediaload;
-}
-
-void close()
-{
-        for( int i = 0; i < KEY_TOTAL; ++i )
-	{
-		SDL_FreeSurface( keysurface[ i ] );
-		keysurface[ i ] = NULL;
-	}      
-        SDL_DestroyWindow(window);
-        window=NULL;
 
 }
-int main( int argc , char* argv[] ){
-        SDL_Rect stretchRect;
-                stretchRect.x = 0;
-                stretchRect.y = 0;
-                stretchRect.w = 1000;
-                stretchRect.h = 1000;
-     
-     bool quit = false;
-     SDL_Event e ;
+void init()
+{       
+        //initialize all subsystems
+        SDL_Init(SDL_INIT_EVERYTHING);
+        IMG_Init(IMG_INIT_PNG);
+        TTF_Init();
 
-     if(init())cout<<"INITIALIZED"<<endl;
-     if(loadmedia())cout<<"LOADED MEDIA"<<endl;
+}
 
-      images = keysurface[KEY_DEFAULT];
-      
-     while(!quit)
-     {
-        while(SDL_PollEvent( &e ) != 0)
-        {
-                if( e.type == SDL_QUIT ) quit=true ;
-                else if( e.type == SDL_KEYDOWN )//this event happens when any key on keyboard is pressed
-                  {
-                     switch(e.key.keysym.sym)
-                     {       
-                           case SDLK_UP:
-                           images = keysurface[KEY_UP];
-                           break;
-                           case SDLK_DOWN:
-                           images = keysurface[KEY_DOWN];
-                           break;
-                           case SDLK_LEFT:
-                           images = keysurface[KEY_LEFT];
-                           break;
-                           case SDLK_RIGHT:
-                           images = keysurface[KEY_RIGHT];
-                           break;
-
-                           default:
-                           images = keysurface[KEY_DEFAULT];
-                           break;
-                     }
-                  }
+class texrect{
+       
+       public: 
+       int x,y,h,w;
+       
+       texrect()//default constructer
+       {
+           x=0;
+           y=0;
+           h=0;
+           w=0;       
        }
 
-        SDL_BlitScaled( images , NULL , wsurface , &stretchRect );
-        SDL_UpdateWindowSurface( window );     
-     }
-         
-        close();
+       texrect( int x , int y , int height , int width )//paremeterised constructor
+       {
+        rectangle.x=x;
+        rectangle.y=y;
+        rectangle.h=height;
+        rectangle.w=width;
+       }
 
-return 0;
+       
+       void loadtexture( std::string path)//loading texture from a path
+       {
+
+        SDL_Surface* temp=IMG_Load( path.c_str() );
+        text = SDL_CreateTextureFromSurface( renderer , temp );
+        SDL_FreeSurface(temp);
+        SDL_RenderCopy(renderer , text , NULL , &rectangle );
+
+       }
+       
+       void update() // updating position of rectangle and updating to renderer
+       {      
+               rectangle.x=x;
+               rectangle.y=y;
+               SDL_RenderCopy(renderer , text , NULL , &rectangle );
+       }
+       
+       private :
+
+       SDL_Rect rectangle;
+       SDL_Texture* text;
+       
+};
+
+class Sigma:public texrect
+{
+      
+      
+}
+
+void update_display();
+
+void show_display()
+{
+        SDL_RenderPresent( renderer );
+}
+
+keypress input( SDL_Event e )
+{
+           if( e.type = SDL_KEYDOWN )
+           {
+                 switch(e.key.keysym.sym)
+                 {
+                     
+
+
+                 }
+
+           }
+}
+
+
+int main ( int argc , char* argv[] )
+{
+        
+        init();
+        
+        bool quit=false;
+        SDL_Event e;
+         
+        while(!quit) //gameloop
+        {
+        
+        while(SDL_PollEvent(&e) )
+        {
+                   if( e.type == SDL_QUIT ) quit = true;
+
+                   else if ( e.type == SDL_KEYDOWN )
+                   {
+                        input(e);
+                   }
+        }   
+                  
+                   update_display();
+                   show_display();
+
+        } 
+
+
+        return 0;
+
 }
